fd48da0ab59d1fc192a9fa25f1fc837c5cb31368
aprilchuie <aprilhuie@dhcp-v011-174.mobile.uci.edu>
2017-11-16 17:04:48 -0800

    Add Lightbox modal
    - Update style.css
    - Update index.html
    - Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,27 @@ function mobileNav() {
     }
 }
 
+function openModal() {
+	document.getElementById('illustrationModal').style.display="block";
+}
+function closeModal() {
+	document.getElementById('illustrationModal').style.display="none";
+}
+function currentImg(n) {
+	showSlides(slideIndex = n);
+}
+var workImageIndex = 1;
+showSlides(workImageIndex);
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("work-images");
+	var captionText = document.getElementById("caption");
+	slides[n].style.display = "block";
+	// for loop through slides to display none and insert into slides array
+	// slides[n].style.display = "block";
+	// captionText.innerHTML
+}
+
 function backToTop() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
