b4768e45cf60d765e5963a6df8d36c1cde3ffbd2
aprilchuie <aprilhuie@dhcp-v063-061.mobile.uci.edu>
2017-11-30 15:47:29 -0800

    Trying to fix responsive nav with header tag
    Update index.html
    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function mobileNav() {
-    var x = document.getElementsByTagName("nav");
+    var x = document.getElementById("topNav");
     if (x.className === "top-nav") {
         x.className += " responsive";
     } else {
