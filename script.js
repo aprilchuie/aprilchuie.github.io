002650b0d5b7761dd1dd66ce4b08296cd3553036
aprilchuie <aprilhuie@MacBook-Air-CH.local>
2017-12-01 19:09:48 -0800

    Update index.html Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function mobileNav() {
 function openModal(modalName) {
 	document.getElementById(modalName).style.display="block";
 }
-function closeModal() {
+function closeModal(modalName) {
 	document.getElementById(modalName).style.display="none";
 }
 function currentImg(n) {
