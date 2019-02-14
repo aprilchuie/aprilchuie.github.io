76a9f84aa378b2aea786add66ea69b0dffb155a0
aprilchuie <aprilhuie@MacBook-Air-CH.local>
2019-02-13 19:16:57 -0800

    Switch back to hideShow

--- script.js
+++ script.js
@@ -41,23 +41,6 @@ function hideShow(id) {
         x.style.display = "none";
     }
 }
-// toggle for collapsible elements
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-      this.innerHTML = "Open";
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-      this.innerHTML = "Close";
-    }
-  });
-}
 
 function backToTop() {
 	document.body.scrollTop = 0;
